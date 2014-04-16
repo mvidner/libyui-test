93cc2f1dc43825c6e72029801feb47428ce40b2f
Martin Vidner <mvidner@suse.cz>
2014-04-16 15:17:04 +0200

    dialog_cleanup_test: do destroy manually
    ncurses has problems with leftovers and qt needs fixing first

--- dialog_cleanup_test.cc
+++ dialog_cleanup_test.cc
@@ -1,14 +1,14 @@
 
 #include <YUI.h>
+#include <YDialog.h>
 #include <YWidgetFactory.h>
 
 int main( int argc, char **argv )
 {
     // Initialize the UI
     YWidgetFactory * wf = YUI::widgetFactory();
-    // Open two dialogs but don't close them.
-    // A global cleanup routine should handle the closing, without crashing.
-    /* YDialog * d1 = */ wf->createPopupDialog();
-    /* YDialog * d2 = */ wf->createPopupDialog();
+
+    YDialog * d1 = wf->createPopupDialog();
+    d1->destroy();
     return 0;
 }
